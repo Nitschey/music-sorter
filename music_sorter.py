4e5f953a6ccc28b95011970661b7bf4a6e0bf132
Nitschi <gnniklasgames@gmail.com>
2026-06-29 15:34:50 +0200

    fixed some spacing & added comments to clarify constants

--- music_sorter.py
+++ music_sorter.py
@@ -3,12 +3,13 @@
 from pathlib import Path
 from shutil import move
 
-# list of fluff that can appear in YT music videos
+# list of fluff that can appear in YT music videos -> just removed
+# not exhaustive, some arcane title constructions will still need manual editing
 FLUFF_LIST = ["hd", "official", "music", "content", "video", "visual", "visualizer", "visualiser", 
             "4k", "upgrade", "lyric", "lyrics" "(", ")", "[", "]" "@"]
-# structural & grammar constructions that get normalised by yt-dlp and need cleaning
+# structural & grammar constructions that get created by yt-dlp normalising file names -> changed/cleaned
 STRUCTURES_LIST = [("_-_", "-"), ("_s_", "s_"), ("_re_", "re_"), ("_d_", "d_"), ("_&_", "_and_")]
-# formats to handle 
+# formats to handle
 AUDIO_FORMATS = ("opus", "m4a", "mp3", "flac", "ogg", "wav", "webm")
 
 def create_bands_set(music_dir: Path) -> set:
@@ -47,7 +48,7 @@ def sort_song(song: Path, bands: set) -> bool:
                 return was_sorted
             except:
                 print(f"[warning] couldn't sort {song.name}")
-                return was_sorted       
+                return was_sorted
     return was_sorted
 
 def main() -> None:
@@ -58,7 +59,7 @@ def main() -> None:
         try:
             music_dir = Path(user_input).resolve()
         except:
-            print("[error] that path doesn't exist or you do nt have permissions for it :(")
+            print("[error] that path doesn't exist or you don't have permissions for it :(")
             raise SystemExit("[error] execute me again with a valid path pls")
     bands = create_bands_set(music_dir)
     print(bands)
